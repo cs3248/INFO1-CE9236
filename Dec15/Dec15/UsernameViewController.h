1c2d06cc847d211cae83636748cdeb4b74afe1d9
Christopher Severino <christopher@severino.com>
2011-12-15 01:57:39 -0500

    Dec 15 - WebWall initial check in

---
+++ Dec15/Dec15/UsernameViewController.h
@@ -0,0 +1,16 @@
+//
+//  UsernameViewController.h
+//  Dec15
+//
+//  Created by Severino Christopher on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsernameViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+
+
+@end
